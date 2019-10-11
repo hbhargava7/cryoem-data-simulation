fb1ec17d6a15856cb2f77bfcb7fe7769f0323c87
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-11 09:58:39 -0700

    analyze_alignment.py working

--- analyze_alignment.py
+++ analyze_alignment.py
@@ -16,15 +16,22 @@ mpl.rcParams['figure.dpi'] = 100
 plt.style.use(['dark_background'])
 
 def main(args):
-	which = args['filename']
-	path = args['filepath']
+	which = args.inputname
+	path = args.inputpath
 	# Read the data
-	print('Reading theoretical data...')
+	print('reading theoretical data...')
 	temet_theoretical = read_theoretical(path+'temet_' + which + '.star')
 	wt_theoretical = read_theoretical(path+'wt_' + which + '.star')
-	print('Reading experimental data...')
+	assert len(temet_theoretical) == len(wt_theoretical), 'theoretical starfiles have inconsistent lengths'
+
+	print('reading experimental data...')
 	temet_experimental = read_experimental(path+'temet_' + which + '.par')
 	wt_experimental = read_experimental(path+'wt_' + which + '.par')
+	assert len(temet_experimental) == len(wt_experimental), 'experimental parfiles have inconsistent lengths'
+
+	assert len(temet_experimental) == len(temet_theoretical), 'experimental and theoretical particle files have inconsistent lengths'
+
+	print('read %d particles' % len(temet_theoretical))
 
 	# Compute angle errors
 	print('computing angular errors...')
@@ -57,7 +64,7 @@ def main(args):
 	ax1.title.set_text('Angle Errors')
 	ax1.set_xlabel("Error Distance (°)")
 	ax1.set_ylabel("Number of Errors")
-	ax1.text(0.1,0.7,"TeMet MAE: %.2f \nWT MAE: %.2f" % (temet_angle_me, wt_angle_mae),transform=ax.transAxes)
+	ax1.text(0.1,0.7,"TeMet MAE: %.2f \nWT MAE: %.2f" % (temet_angle_me, wt_angle_mae),transform=ax1.transAxes)
 	ax1.legend()
 
 	# Plot position errors
@@ -66,7 +73,9 @@ def main(args):
 	ax2.title.set_text('Position Errors')
 	ax2.set_xlabel("Error Distance (Å)")
 	ax2.set_ylabel("Number of Errors")
-	ax2.text(.55,0.7,"TeMet MAE: %.2f \nWT MAE: %.2f" % (temet_position_mae, wt_position_mae),transform=ax.transAxes)
+	ax2.text(.1,0.7,"TeMet MAE: %.2f \nWT MAE: %.2f" % (temet_position_mae, wt_position_mae),transform=ax2.transAxes)
+
+	'displaying plots...'
 
 	ax2.legend()
 	plt.savefig('errors.png')
@@ -76,82 +85,82 @@ def main(args):
 
 
 def read_theoretical(path):
-    # Read the theoretical starfile
-    # We only want (1-indexed): 2 (psi), 3 (phi), 4 (theta), 12 (originX), 13 (originY) 
-    # BEWARE skiprows, starfile header lengths may vary
-    theoretical = pd.read_csv(path, delim_whitespace=True, header=None, skiprows=21)
-    theoretical = theoretical[theoretical.columns[[1, 2, 3, 11, 12]]]
-    theoretical.columns = [ 'psi', 'phi', 'theta', 'shiftX', 'shiftY']
-    theoretical = theoretical.astype(float)
-    theoretical['quaternion'] = theoretical.apply(lambda row: euler2quat(row.phi*np.pi/180, row.theta*np.pi/180, row.psi*np.pi/180), axis=1)
-    return theoretical
+	# Read the theoretical starfile
+	# We only want (1-indexed): 2 (psi), 3 (phi), 4 (theta), 12 (originX), 13 (originY) 
+	# BEWARE skiprows, starfile header lengths may vary
+	theoretical = pd.read_csv(path, delim_whitespace=True, header=None, skiprows=21, low_memory=False)
+	theoretical = theoretical[theoretical.columns[[1, 2, 3, 11, 12]]]
+	theoretical.columns = [ 'psi', 'phi', 'theta', 'shiftX', 'shiftY']
+	theoretical = theoretical.astype(float)
+	theoretical['quaternion'] = theoretical.apply(lambda row: euler2quat(row.phi*np.pi/180, row.theta*np.pi/180, row.psi*np.pi/180), axis=1)
+	return theoretical
 
 def read_experimental(path):
-    # Read the experimental parfile
-    # BEWARE dropping last two rows.
-    experimental = pd.read_csv(path, delim_whitespace=True)
-    experimental = experimental[experimental.columns[[1, 3, 2, 4, 5]]]
-    experimental.columns = [ 'psi', 'phi', 'theta', 'shiftX', 'shiftY']
-    experimental.drop(experimental.tail(2).index,inplace=True)
-    experimental = experimental.astype(float)
-    experimental['quaternion'] = experimental.apply(lambda row: euler2quat(row.phi*np.pi/180, row.theta*np.pi/180, row.psi*np.pi/180), axis=1)
-    return experimental
+	# Read the experimental parfile
+	# BEWARE dropping last two rows.
+	experimental = pd.read_csv(path, delim_whitespace=True, low_memory=False)
+	experimental = experimental[experimental.columns[[1, 3, 2, 4, 5]]]
+	experimental.columns = [ 'psi', 'phi', 'theta', 'shiftX', 'shiftY']
+	experimental.drop(experimental.tail(2).index,inplace=True)
+	experimental = experimental.astype(float)
+	experimental['quaternion'] = experimental.apply(lambda row: euler2quat(row.phi*np.pi/180, row.theta*np.pi/180, row.psi*np.pi/180), axis=1)
+	return experimental
 
 def euler2quat(alpha, beta, gamma):
-    ha, hb, hg = alpha / 2, beta / 2, gamma / 2
-    ha_p_hg = ha + hg
-    hg_m_ha = hg - ha
-    q = np.array([np.cos(ha_p_hg) * np.cos(hb),
-                  np.sin(hg_m_ha) * np.sin(hb),
-                  np.cos(hg_m_ha) * np.sin(hb),
-                  np.sin(ha_p_hg) * np.cos(hb)])
-    return q
+	ha, hb, hg = alpha / 2, beta / 2, gamma / 2
+	ha_p_hg = ha + hg
+	hg_m_ha = hg - ha
+	q = np.array([np.cos(ha_p_hg) * np.cos(hb),
+				  np.sin(hg_m_ha) * np.sin(hb),
+				  np.cos(hg_m_ha) * np.sin(hb),
+				  np.sin(ha_p_hg) * np.cos(hb)])
+	return q
 
 # Quaternion to Euler Angles (from https://github.com/asarnow/pyem geom.py)
 def quat2euler(q):
-    ha1 = np.arctan2(q[1], q[2])
-    ha2 = np.arctan2(q[3], q[0])
-    alpha = ha2 - ha1  # np.arctan2(r21/r20)
-    beta = 2 * np.arccos(np.sqrt(q[0]**2 + q[3]**2))  # np.arccos*r33
-    gamma = ha1 + ha2  # np.arctan2(r12/-r02)
-    return alpha, beta, gamma
+	ha1 = np.arctan2(q[1], q[2])
+	ha2 = np.arctan2(q[3], q[0])
+	alpha = ha2 - ha1  # np.arctan2(r21/r20)
+	beta = 2 * np.arccos(np.sqrt(q[0]**2 + q[3]**2))  # np.arccos*r33
+	gamma = ha1 + ha2  # np.arctan2(r12/-r02)
+	return alpha, beta, gamma
 
 # Angular distance between two quaternions
 def quatDist(a,b):
-    # Check to verify that quaternions are unit lengths
-    assert abs(math.sqrt(a[0]**2+a[1]**2+a[2]**2+a[3]**2)-1)<.001,"a is not a unit quaternion"
-    assert abs(math.sqrt(b[0]**2+b[1]**2+b[2]**2+b[3]**2)-1)<.001,"b is not a unit quaternion"
-    
-    # Compute distance
-    s = a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3]
-    s = 2*(s**2)-1
-    return np.arccos(s)*180/np.pi
+	# Check to verify that quaternions are unit lengths
+	assert abs(math.sqrt(a[0]**2+a[1]**2+a[2]**2+a[3]**2)-1)<.001,"a is not a unit quaternion"
+	assert abs(math.sqrt(b[0]**2+b[1]**2+b[2]**2+b[3]**2)-1)<.001,"b is not a unit quaternion"
+	
+	# Compute distance
+	s = a[0]*b[0] + a[1]*b[1] + a[2]*b[2] + a[3]*b[3]
+	s = 2*(s**2)-1
+	return np.arccos(s)*180/np.pi
 
 # Given two ordered lists of quaternions, compute distances between each angle
 def computeAngleErrors(A, B):
-    qq = zip(A, B)
-    errors = []
-    for i,v in enumerate(qq):
-        dist = quatDist(v[0],v[1])
-        errors.append((dist))
+	qq = zip(A, B)
+	errors = []
+	for i,v in enumerate(qq):
+		dist = quatDist(v[0],v[1])
+		errors.append((dist))
 
-    return errors
+	return errors
 
 # A and B are (x,y) tuples
 def euclideanDistance(A,B):
-    return math.sqrt((A[0]-B[0])**2 + (A[1]-B[1])**2)
+	return math.sqrt((A[0]-B[0])**2 + (A[1]-B[1])**2)
 
 # theoretical and experimental are arrays of (x,y) tuples
 def computeShiftErrors(theoretical, experimental):
-    ab = zip(theoretical, experimental)
-    errors = []
-    for i, v in enumerate(ab):
-        dist = euclideanDistance(v[0],v[1])
-        errors.append(dist)
-    return errors
+	ab = zip(theoretical, experimental)
+	errors = []
+	for i, v in enumerate(ab):
+		dist = euclideanDistance(v[0],v[1])
+		errors.append(dist)
+	return errors
 
 def mean(array):
-    return sum(array)/len(array)
+	return sum(array)/len(array)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
