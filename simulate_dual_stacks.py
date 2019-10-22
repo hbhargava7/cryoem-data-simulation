ef2f9ee4e186c890d04f4c3732b001f965bd1058
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-21 23:22:14 -0700

    Dual Stacks Working

--- simulate_dual_stacks.py
+++ simulate_dual_stacks.py
@@ -72,12 +72,7 @@ def main(args):
 
 	# particle parameters
 	params['n_particles'] = args.n_particles
-
-	params['snr'] = 0.05
-
-	if args.snr is not None:
-		params['snr'] = args.snr
-
+	
 	# miscellaneous parameters
 	params['kernel'] = 'lanczos'
 	params['ksize'] = int(6)
@@ -86,31 +81,35 @@ def main(args):
 	params['bfactor'] = 50.0
 
 	# Read the volume data and compute fft
-	vol,hdr = mrc.readMRC(args.input, inc_header=True)
+	print('Volume metadata will be read from the WT volume.')
+	vol_wt,hdr_wt = mrc.readMRC(args.input_wt, inc_header=True)
+	vol_temet,hdr_temet = mrc.readMRC(args.input_temet, inc_header=True)
 
-	params['boxSize'] = int(vol.shape[0])
-	params['pxSize'] = (hdr['xlen']/hdr['nx'])
+	params['boxSize'] = int(vol_wt.shape[0])
+	params['pxSize'] = (hdr_wt['xlen']/hdr_wt['nx'])
 
 	premult = cryoops.compute_premultiplier(params['boxSize'], params['kernel'], params['ksize']) 
 
-	V = density.real_to_fspace(premult.reshape((1,1,-1)) * premult.reshape((1,-1,1)) * premult.reshape((-1,1,1)) * vol)
+	V_wt = density.real_to_fspace(premult.reshape((1,1,-1)) * premult.reshape((1,-1,1)) * premult.reshape((-1,1,1)) * vol_wt)
+	V_temet = density.real_to_fspace(premult.reshape((1,1,-1)) * premult.reshape((1,-1,1)) * premult.reshape((-1,1,1)) * vol_temet)
 
-	# Compute the mean of the signal, excluding zeros
-	data = vol
-	data[data == 0] = n.nan
-	signal_mean = n.nanmean(data)
-	params['signal_mean'] = signal_mean
-	params['sigma_noise'] = signal_mean/params['snr']
+	params['wt_signal_mean'] = signalMean(vol_wt)
+	params['temet_signal_mean'] = signalMean(vol_temet)
 
-	if args.sigma_noise is not None:
-		params['sigma_noise'] = args.sigma_noise
-		print('Using user-specified sigma_noise.')
+	params['sigma_noise'] = args.sigma_noise
 
 	print('Noise Sigma: ' + str(params['sigma_noise']))
 
+	params['wt_snr'] = params['wt_signal_mean']/params['sigma_noise']
+	params['temet_snr'] = params['temet_signal_mean']/params['sigma_noise']
+
 	# Set up the particles datastructures
-	particles = n.empty((params['n_particles'], params['boxSize'], params['boxSize']), dtype=density.real_t)
-	starfile = []
+	wt_particles = n.empty((params['n_particles'], params['boxSize'], params['boxSize']), dtype=density.real_t)
+	wt_starfile = []
+	
+	temet_particles = n.empty((params['n_particles'], params['boxSize'], params['boxSize']), dtype=density.real_t)
+	temet_starfile = []
+
 	TtoF = sincint.gentrunctofull(N=params['boxSize'], rad=params['rad'])
 	
 	tic = time.time()
@@ -119,9 +118,13 @@ def main(args):
 	lastChunkSize = params['n_particles'] - ((nChunks - 1)*1000)
 
 	# Make a directory to cache data on the disk.
-	tempPath = args.output_path + 'tmp/'
-	if not os.path.exists(tempPath):
-		os.mkdir(tempPath)
+	wt_tempPath = args.output_path + 'wt_tmp/'
+	if not os.path.exists(wt_tempPath):
+		os.mkdir(wt_tempPath)
+
+	temet_tempPath = args.output_path + 'temet_tmp/'
+	if not os.path.exists(temet_tempPath):
+		os.mkdir(temet_tempPath)
 
 	for i in range(nChunks):
 		ticc = time.time()
@@ -132,7 +135,8 @@ def main(args):
 
 		# PROCESS IMPLEMENTATION
 		manager = mp.Manager()
-		output = manager.list()
+		output_wt = manager.list()
+		output_temet = manager.list()
 
 		jobs = []
 		concurrency = mp.cpu_count() - 1
@@ -142,12 +146,12 @@ def main(args):
 
 		sema = mp.Semaphore(concurrency)
 
-		print("\nSimulating %d particles on %d processors." % (params['n_particles'], concurrency))
+		print("\nSimulating %d particles FOR EACH VOLUME on %d processors." % (params['n_particles'], concurrency))
 
 		for j in range(chunkSize):
 			idx = i * 1000 + j
 			sema.acquire()
-			p = mp.Process(target=simulateParticle, args=(output, params, V, TtoF, idx, tic, sema))
+			p = mp.Process(target=simulateParticles, args=(output_wt, output_temet, params, V_wt, V_temet, TtoF, idx, tic, sema))
 			jobs.append(p)
 			p.start()
 
@@ -155,53 +159,67 @@ def main(args):
 			proc.terminate()
 			proc.join()
 
-		chunkFileName = tempPath + ('%d_chunk.tmp' % i)
-		with open(chunkFileName, 'wb') as filehandle:
-			pickle.dump(list(output), filehandle)
+		wt_chunkFileName = wt_tempPath + ('%d_chunk.tmp' % i)
+		temet_chunkFileName = temet_tempPath + ('%d_chunk.tmp' % i)
+
+		with open(wt_chunkFileName, 'wb') as filehandle:
+			pickle.dump(list(output_wt), filehandle)
 			filehandle.close()
 
-		print("\nDone simulating chunk %d of size %d in time %s." % (i+1, chunkSize, format_timedelta(time.time() - ticc)))
+		with open(temet_chunkFileName, 'wb') as filehandle:
+			pickle.dump(list(output_temet), filehandle)
+			filehandle.close()
 
-	# Join the chunks together
-	results = []
-	chunkFiles = [f for f in os.listdir(tempPath) if os.path.isfile(os.path.join(tempPath, f))]
-	tempPath = os.path.abspath(tempPath)
-	for f in chunkFiles:
-		file = open(os.path.join(tempPath, f), 'rb')
-		chunk = pickle.load(file)
-		results.extend(chunk)
+		# print("\nDone simulating chunk %d of size %d in time %s." % (i+1, chunkSize, format_timedelta(time.time() - ticc)))
 
-	# Delete the temp directory
-	shutil.rmtree(tempPath)
 	print("\nDone simulating all particles in: %s" % format_timedelta(time.time() - tic))
-	print("\nRate of simulation: %.2f particles per second." % (int(params['n_particles'])/float(time.time() - tic)))
+	print("\nRate of simulation: %.2f particles PAIRS per second." % (int(params['n_particles'])/float(time.time() - tic)))
+
 	simulation_rate = int(params['n_particles'])/float(time.time() - tic)
 
-	results = sorted(results, key=lambda x: x[0])
-
-	particles = [result[1] for result in results]
-	starfile = [result[2] for result in results]
-
 	print('\nWriting out data...')
+
+	particles_wt, starfile_wt = processResultsFromChunkPath(wt_tempPath)
+	particles_temet, starfile_temet = processResultsFromChunkPath(temet_tempPath)
 
 	# Plot the first 8 images
 	fig = plt.figure(figsize=(12, 5))
 	col = 4
 	row = 2
 	for i in range(1, col*row +1):
-		img = particles[i]
+		img = particles_wt[i]
 		fig.add_subplot(row, col, i)
 		plt.imshow(img, cmap='gray')
-	plt.savefig(args.output_path + 'plot.png')
+	plt.savefig(args.output_path + 'wt_plot.png')
+	# Plot the first 8 images
+	fig = plt.figure(figsize=(12, 5))
+	col = 4
+	row = 2
+	for i in range(1, col*row +1):
+		img = particles_temet[i]
+		fig.add_subplot(row, col, i)
+		plt.imshow(img, cmap='gray')
+	plt.savefig(args.output_path + 'temet_plot.png')
 
-	mrc.writeMRC(args.output_path + 'simulated_particles.mrcs', n.transpose(particles,(1,2,0)), params['pxSize'])
+
+	mrc.writeMRC(args.output_path + 'wt_simulated_particles.mrcs', n.transpose(wt_particles,(1,2,0)), params['pxSize'])
+	mrc.writeMRC(args.output_path + 'temet_simulated_particles.mrcs', n.transpose(temet_particles,(1,2,0)), params['pxSize'])
 
 	# Write the starfile
-	f = open((args.output_path + 'simulated_particles.star'), 'w')
+	f = open((args.output_path + 'wt_simulated_particles.star'), 'w')
 	# Write the header
 	f.write("\ndata_images\n\nloop_\n_rlnAmplitudeContrast #1 \n_rlnAnglePsi #2 \n_rlnAngleRot #3 \n_rlnAngleTilt #4 \n_rlnClassNumber #5 \n_rlnDefocusAngle #6 \n_rlnDefocusU #7 \n_rlnDefocusV #8 \n_rlnDetectorPixelSize #9 \n_rlnImageName #10 \n_rlnMagnification #11 \n_rlnOriginX #12 \n_rlnOriginY #13 \n_rlnPhaseShift #14 \n_rlnSphericalAberration #15\n_rlnVoltage #16\n\n")
 	# Write the particle information
-	for l in starfile:
+	for l in starfile_wt:
+		f.write(' '.join(l) + '\n')
+	f.close()
+
+	# Write the starfile
+	f = open((args.output_path + 'temet_simulated_particles.star'), 'w')
+	# Write the header
+	f.write("\ndata_images\n\nloop_\n_rlnAmplitudeContrast #1 \n_rlnAnglePsi #2 \n_rlnAngleRot #3 \n_rlnAngleTilt #4 \n_rlnClassNumber #5 \n_rlnDefocusAngle #6 \n_rlnDefocusU #7 \n_rlnDefocusV #8 \n_rlnDetectorPixelSize #9 \n_rlnImageName #10 \n_rlnMagnification #11 \n_rlnOriginX #12 \n_rlnOriginY #13 \n_rlnPhaseShift #14 \n_rlnSphericalAberration #15\n_rlnVoltage #16\n\n")
+	# Write the particle information
+	for l in starfile_temet:
 		f.write(' '.join(l) + '\n')
 	f.close()
 
@@ -209,9 +227,11 @@ def main(args):
 	f = open((args.output_path + 'simulation_metadata.txt'), 'w')
 	f.write("Thank you for using this data simulator.\n")
 	f.write("https://github.com/hbhargava7/cryoem-data-simulation\n\n")
-	f.write("Simulated %d particles in %s.\n" % (params['n_particles'], format_timedelta(time.time() - tic)))
-	f.write("Rate of simulation was %.2f particles per second." % simulation_rate)
-	f.write("\n\nInput volume: %s.\n" % args.input)
+	f.write("Simulated %d particle pairs in %s.\n" % (params['n_particles'], format_timedelta(time.time() - tic)))
+	f.write("Rate of simulation was %.2f particle PAIRS per second." % simulation_rate)
+	f.write("\n\nInput wt volume: %s.\n" % args.input_wt)
+	f.write("\n\nInput temet volume: %s.\n" % args.input_temet)
+
 	f.write("Output path: %s.\n\n" % args.output_path)
 
 	if args.sigma_noise is not None:
@@ -225,6 +245,32 @@ def main(args):
 	f.close()
 
 	print('Done!')
+
+def processResultsFromChunkPath(tempPath):
+	results = []
+	chunkFiles = [f for f in os.listdir(tempPath) if os.path.isfile(os.path.join(tempPath, f))]
+	tempPath = os.path.abspath(tempPath)
+
+	for f in chunkFiles:
+		file = open(os.path.join(tempPath, f), 'rb')
+		chunk = pickle.load(file)
+		results.extend(chunk)
+
+	# Delete the temp directory
+	shutil.rmtree(tempPath)
+
+	results = sorted(results, key=lambda x: x[0])
+
+	particles = [result[1] for result in results]
+	starfile = [result[2] for result in results]
+
+	return particles,starfile
+def signalMean(volume):
+	# Compute the mean of the signal, excluding zeros
+
+	nonzero = volume
+	nonzero[nonzero == 0] = n.nan
+	return n.nanmean(nonzero)
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -259,10 +305,10 @@ def query_yes_no(question, default="yes"):
                              "(or 'y' or 'n').\n")
 
 
-def simulateParticle(output,params, V, TtoF, i, tic, sema):
+def simulateParticles(output_wt, output_temet,params, V_wt, V_temet, TtoF, i, tic, sema):
 	ellapse_time = time.time() - tic
 	remain_time = float(params['n_particles'] - i)*ellapse_time/max(i,1)
-	print("\r%.2f Percent Complete (%d particles done)... (Elapsed: %s, Remaining: %s)" % (i/float(params['n_particles'])*100.0,i+1,format_timedelta(ellapse_time),format_timedelta(remain_time)), end="")
+	print("\r%.2f Percent Complete (%d particle pairs done)... (Elapsed: %s, Remaining: %s)" % ((i+1)/float(params['n_particles'])*100.0,i+1,format_timedelta(ellapse_time),format_timedelta(remain_time)), end="")
 		
 	# Numpy random seed
 	n.random.seed(int.from_bytes(os.urandom(4), byteorder='little')) 
@@ -298,19 +344,31 @@ def simulateParticle(output,params, V, TtoF, i, tic, sema):
 	R = geom.rotmat3D_EA(*EA)[:,0:2]
 	slop = cryoops.compute_projection_matrix([R], params['boxSize'], params['kernel'], params['ksize'], params['rad'], 'rots')
 	S = cryoops.compute_shift_phases(shift.reshape((1,2)), params['boxSize'], params['rad'])[0]
-	D = slop.dot(V.reshape((-1,)))
-	D *= S
+
+
+	D_wt = slop.dot(V_wt.reshape((-1,)))
+	D_wt *= S
+
+	D_temet = slop.dot(V_temet.reshape((-1,)))
+	D_temet *= S
 	  
 	# Generate the CTF
 	C = ctf.compute_full_ctf(None, params['boxSize'], params['pxSize'], params['accel_kv'], params['spherical_abberr'], params['amp_contrast'], p['defocus_a'], p['defocus_b'], n.radians(p['astig_angle']), 1, params['bfactor'])
 
 	# Apply CTF to the projection and write to particles array
-	ctf_distorted = density.fspace_to_real((C*TtoF.dot(D)).reshape((params['boxSize'],params['boxSize'])))
-	noise_added = ctf_distorted + n.require(n.random.randn(params['boxSize'], params['boxSize'])*params['sigma_noise'],dtype=density.real_t)
-	particle = -noise_added
-	
+	wt_ctf_distorted = density.fspace_to_real((C*TtoF.dot(D_wt)).reshape((params['boxSize'],params['boxSize'])))
+	temet_ctf_distorted = density.fspace_to_real((C*TtoF.dot(D_temet)).reshape((params['boxSize'],params['boxSize'])))
+
+	noise = n.require(n.random.randn(params['boxSize'], params['boxSize'])*params['sigma_noise'],dtype=density.real_t)
+
+	wt_noise_added = wt_ctf_distorted + noise
+	temet_noise_added = temet_ctf_distorted + noise
+
+	wt_particle = -wt_noise_added
+	temet_particle = -temet_noise_added
+
 	# Save the particle parameters for the star file
-	starfile_line = [str(params['amp_contrast']), 
+	wt_starfile_line = [str(params['amp_contrast']), 
 					 str(p['psi']), 
 					 str(p['phi']), 
 					 str(p['theta']),
@@ -319,7 +377,7 @@ def simulateParticle(output,params, V, TtoF, i, tic, sema):
 					 str(p['defocus_a']),
 					 str(p['defocus_b']),
 					 str(params['pxSize']),
-					 "%d@/simulated_particles.mrcs" % (i+1),
+					 "%d@/wt_simulated_particles.mrcs" % (i+1),
 					 str(params['mag']),
 					 str(0),
 					 str(0),
@@ -327,17 +385,35 @@ def simulateParticle(output,params, V, TtoF, i, tic, sema):
 					 str(params['spherical_abberr']),
 					 str(params['accel_kv'])]
 
-	output.append((i, particle, starfile_line))
+	temet_starfile_line = [str(params['amp_contrast']), 
+				 str(p['psi']), 
+				 str(p['phi']), 
+				 str(p['theta']),
+				 str(1),
+				 str(p['astig_angle']),                     
+				 str(p['defocus_a']),
+				 str(p['defocus_b']),
+				 str(params['pxSize']),
+				 "%d@/temet_simulated_particles.mrcs" % (i+1),
+				 str(params['mag']),
+				 str(0),
+				 str(0),
+				 str(0),
+				 str(params['spherical_abberr']),
+				 str(params['accel_kv'])]
+
+	output_wt.append((i, wt_particle, wt_starfile_line))
+	output_temet.append((i, temet_particle, temet_starfile_line))
+
 	sema.release()
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--input_wt", help="input wild-type 3d volume", type=str)
-	parser.add_argument("--input_temet", help="input telluromethionine-type 3d volume", type=str)
-	parser.add_argument("output_path", type=str)
-	parser.add_argument("--n_particles", help="number of particles to simulate", type=int)
-	parser.add_argument("--sigma_noise", help="noise stdev", type=float)
-	parser.add_argument("--snr", help="signal to noise ratio", type=float)
+	parser.add_argument("--input_wt", help="input wild-type 3d volume", type=str, required=True)
+	parser.add_argument("--input_temet", help="input telluromethionine-type 3d volume", type=str, required=True)
+	parser.add_argument("--output_path", help="output path",type=str, required=True)
+	parser.add_argument("--n_particles", help="number of particles to simulate", type=int, required=True)
+	parser.add_argument("--sigma_noise", help="noise stdev", type=float, required=True)
 	parser.add_argument("--cpus", help="number of processors to use", type=int)
 	parser.add_argument("--overwrite", help="overwrite the target directory if necessary?", action='store_true')
 
